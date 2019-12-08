853e9c5f8825038c23fc0e5dbd5743000bb17b46
Moritz Ruth <dev@moritz-ruth.de>
2019-12-08 18:45:15 +0100

    Fix Nuxt server build

--- nuxt/index.js
+++ nuxt/index.js
@@ -4,6 +4,9 @@ import path from "path";
 export default function KisteModule () {
   const options = this.options.kiste;
 
+  this.options.build.transpile.push("kiste");
+  this.options.build.transpile.push("vue-ripple-directive");
+
   this.addPlugin({
     src: path.resolve(__dirname, "plugin.template.js"),
     options
